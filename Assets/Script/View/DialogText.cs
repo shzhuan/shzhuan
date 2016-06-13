2b19e89efd39688d6b580f50870f809ebae80b6b
JackieRen <rjq445746468@hotmail.com>
2016-06-13 19:40:56 +0800

    add typewriter effect

---
+++ Assets/Script/View/DialogText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace View {
+    public class DialogText : MonoBehaviour {
+
+        public Text textBox = null;
+        public string dialogTxt = null;
+        public float waitTime = 0f;
+
+        void Awake() {
+            textBox.text = "";
+            StartCoroutine("PlayText");
+        }
+
+        IEnumerator PlayText() {
+            foreach (char c in dialogTxt) {
+                textBox.text += c;
+                yield return new WaitForSeconds(waitTime);
+            }
+        }
+
+    }
+}
