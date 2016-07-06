fb88f3658848d66273b4905a12b0897a5a413bb5
JackieRen <rjq445746468@hotmail.com>
2016-07-06 19:41:46 +0800

    add about json script

---
+++ Assets/Script/Ctrl/GameStatus.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[Serializable]
+public class GameStatus {
+    public string gameName;
+    public string version;
+    public bool isStereo;
+    public bool isUseHardWare;
+    public refencenes[] statusList;
+}
+
+[Serializable]
+public class refencenes {
+    public refencenes() {
+        name = "";
+        id = -1;
+    }
+
+    public string name;
+    public int id;
+
+}
---
+++ Assets/Script/Ctrl/WriteJson.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+public class WriteJson : MonoBehaviour {
+    public GameStatus gameStatus;
+    public GameObject[] objectList;
+    void Start() {
+        gameStatus = new GameStatus();
+        gameStatus.statusList = new refencenes[objectList.Length];
+        gameStatus.gameName = "";
+
+        for (int i = 0; i < objectList.Length; i++) {
+            gameStatus.statusList[i] = new refencenes();
+            gameStatus.statusList[i].id = i;
+            gameStatus.statusList[i].name = objectList[i].name;
+        }
+    }
+
+    public void SaveJson() {
+        //string json = JsonUtility.ToJson(gameStatus);
+        //string savePath = Application.dataPath + "/Resources/Test01.json";
+        //File.WriteAllText(savePath, json, Encoding.UTF8);
+    }
+
+}
