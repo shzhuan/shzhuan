eb63947bb1ae82942c88dc778c331a3dc9b06928
JackieRen <rjq445746468@hotmail.com>
2016-07-10 23:59:12 +0800

    edit lobby

---
+++ Assets/Script/View/PanelHome/MissonItem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MissonItem : MonoBehaviour {
+
+    public Image btnImage;
+    public Button button;
+
+    private bool m_isSelected = false;
+
+    public bool IsSelscted
+    {
+        get { return m_isSelected; }
+    }
+
+    public void BtnState(bool isSelected) {
+        m_isSelected = isSelected;
+        if (isSelected) {
+            btnImage.color = new Color(btnImage.color.r, btnImage.color.g, btnImage.color.b, 1);
+            return;
+        }
+        btnImage.color = new Color(btnImage.color.r, btnImage.color.g, btnImage.color.b, 0.8f);
+    }
+
+}
