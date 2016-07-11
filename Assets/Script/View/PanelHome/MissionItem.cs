d029f970ad9d2e332f351543455b373e1ec9d002
JackieRen <rjq445746468@hotmail.com>
2016-07-11 23:39:51 +0800

    mission code supplement

--- Assets/Script/View/PanelHome/MissionItem.cs
+++ Assets/Script/View/PanelHome/MissionItem.cs
@@ -2,30 +2,33 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class MissionItem : MonoBehaviour {
+namespace View.Home {
+    public class MissionItem : MonoBehaviour {
 
-    public Image btnImage;
-    public Text missionDetail;
-    public Button button;
+        public Image btnImage;
+        public Image missionState; 
+        public Text missionDetail;
+        public Button button;
 
-    private bool m_isSelected = false;
+        private bool m_isSelected = false;
 
-    public bool IsSelscted
-    {
-        get { return m_isSelected; }
-    }
-
-    public void BtnState(bool isSelected) {
-        m_isSelected = isSelected;
-        if (isSelected) {
-            btnImage.color = new Color(btnImage.color.r, btnImage.color.g, btnImage.color.b, 1);
-            return;
+        public bool IsSelscted
+        {
+            get { return m_isSelected; }
         }
-        btnImage.color = new Color(btnImage.color.r, btnImage.color.g, btnImage.color.b, 0.8f);
-    }
 
-    public void SetMissionDetail(string detail) {
-        missionDetail.text = detail;
-    }
+        public void BtnState(bool isSelected) {
+            m_isSelected = isSelected;
+            if (isSelected) {
+                btnImage.color = new Color(btnImage.color.r, btnImage.color.g, btnImage.color.b, 1);
+                return;
+            }
+            btnImage.color = new Color(btnImage.color.r, btnImage.color.g, btnImage.color.b, 0.8f);
+        }
 
+        public void SetMissionDetail(string detail) {
+            missionDetail.text = detail;
+        }
+
+    }
 }
--- Assets/Script/View/PanelHome/MissionItemList.cs
+++ Assets/Script/View/PanelHome/MissionItemList.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MissionItemList : MonoBehaviour {
+namespace View.Home {
+    public class MissionItemList : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        public MissionItem[] missionList;
+
+    }
 }
