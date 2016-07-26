8e7be11287a5ad475e5c8e080760b71731fb5baa
JackieRen <rjq445746468@hotmail.com>
2016-07-26 23:45:12 +0800

    add prop data

---
+++ Assets/Script/Model/PropDataList.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Model {
+    public class PropDataList : MonoBehaviour {
+        public PropData[] list;
+
+        public PropData GetProp(string id) {
+            if (string.IsNullOrEmpty(id)) {
+                return null;
+            }
+            int num = int.Parse(id.Substring(1, 3));
+            return list[num];
+        }
+    }
+}
