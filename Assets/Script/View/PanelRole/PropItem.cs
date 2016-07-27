51719b6048d35895ec653b7bf4644d3bc7f93dbb
JackieRen <rjq445746468@hotmail.com>
2016-07-27 23:58:29 +0800

    add about prop code

---
+++ Assets/Script/View/PanelRole/PropItem.cs
@@ -0,0 +1,19 @@
+﻿ using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace View.Role {
+    public class PropItem : MonoBehaviour {
+        public Image icon;
+        public Text propName;
+        public Text num;
+
+        private string m_Id = "";
+
+        public string PropID
+        {
+            get { return m_Id; }
+        }
+
+    }
+}
---
+++ Assets/Script/View/PanelRole/PropItemList.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace View.Role {
+    public class PropItemList : MonoBehaviour {
+        public Object missionPrefab;
+        public RectTransform listScope;
+        public List<PropItem> itemList = new List<PropItem>();
+
+        private const float m_propItemWidth = 0f;
+
+        public void SetListWidth(int propNum) {
+            listScope.sizeDelta = new Vector2(propNum * m_propItemWidth, listScope.sizeDelta.y);
+            listScope.anchoredPosition = Vector2.zero;
+        }
+
+    }
+}
