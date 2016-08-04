6bd4c5678b3a39cc006298e24d8f39cfd2f15c62
JackieRen <rjq445746468@hotmail.com>
2016-08-04 23:27:34 +0800

    time loading

--- Assets/Script/View/PanelHome/PanelHome.cs
+++ Assets/Script/View/PanelHome/PanelHome.cs
@@ -10,6 +10,26 @@ namespace View.Home {
         public Button btnShop;
         public Image mask;
         public Image testImage;
-        
+
+        void Start() {
+            StartCoroutine("TimeLoading");
+        }
+
+        IEnumerator TimeLoading() {
+            while (true) {
+                if (testImage.fillAmount == 0) {
+                    testImage.fillClockwise = true;
+                } else if (testImage.fillAmount == 1) {
+                    testImage.fillClockwise = false;
+                }
+                if (testImage.fillClockwise) {
+                    testImage.fillAmount += 0.05f;
+                } else {
+                    testImage.fillAmount -= 0.05f;
+                }
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+
     }
 }
