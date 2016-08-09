f4a5efba241fa6a41358651e43112dacf379e0fe
JackieRen <rjq445746468@hotmail.com>
2016-08-09 19:11:03 +0800

    init some code

---
+++ Assets/Script/PanelBattle/View/Role.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace View.Battle {
+    public class Role : MonoBehaviour {
+        public Image roleIcon;
+        public Image roleMask;
+
+    }
+}
