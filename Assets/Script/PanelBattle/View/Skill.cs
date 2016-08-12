57e74329da700f31f7dbe787539a59433661b4dc
JackieRen <rjq445746468@hotmail.com>
2016-08-12 19:46:00 +0800

    add effects

---
+++ Assets/Script/PanelBattle/View/Skill.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace View.Battle {
+    public class Skill : MonoBehaviour {
+        public enum Type { FRAME_ANIMATION, PARTICLE }
+        public Type SkillType = Type.FRAME_ANIMATION;
+        public SpriteRenderer spriteRenderer;
+        public List<Sprite> spriteList = new List<Sprite>();
+        public bool isPlay = false;
+        public bool isLoop = false;
+        public float delayTime = 0f;
+        public int frameTime = 24;
+
+        private float m_timeCnt = 0;
+        
+        void Update() {
+            if (isPlay && SkillType == Type.FRAME_ANIMATION) {
+                if (m_timeCnt >= delayTime) {
+                    int frameCnt = (int)((m_timeCnt - delayTime) / frameTime);
+                    if (frameCnt < spriteList.Count) {
+                        spriteRenderer.sprite = spriteList[frameCnt];
+                    }
+                    if (isLoop) {
+                        spriteRenderer.sprite = spriteList[frameCnt % spriteList.Count];
+                    } else {
+                        spriteRenderer.sprite = null;
+                        isPlay = false;
+                        m_timeCnt = 0;
+                    }
+                } else {
+                    m_timeCnt += Time.deltaTime;
+                }
+            }
+        }
+
+    }
+}
