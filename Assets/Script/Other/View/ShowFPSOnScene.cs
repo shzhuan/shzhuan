283c717271b6a729243f3e76b17eed0b350ed72b
Jackie Ren <Jackie Ren>
2016-10-02 23:55:08 +0800

    add TimeManager and ShowFPSOnScene

---
+++ Assets/Script/Other/View/ShowFPSOnScene.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowFPSOnScene : MonoBehaviour {
+    
+    private int m_frames = 0;
+    private float m_oldTime = 0f;
+    private float m_fps;
+
+    public const float m_intervalTime = 0.5F;
+
+    void Awake() {
+        Application.targetFrameRate = 30;
+    }
+
+    void Start() {
+        m_oldTime = Time.realtimeSinceStartup;
+        m_frames = 0;
+    }
+
+    void Update() {
+        ++m_frames;
+        if (Time.realtimeSinceStartup > m_oldTime + m_intervalTime) {
+            m_fps = m_frames / (Time.realtimeSinceStartup - m_oldTime);
+            m_frames = 0;
+            m_oldTime = Time.realtimeSinceStartup;
+        }
+    }
+
+    void OnGUI() {
+        GUILayout.Label("FPS:" + m_fps.ToString("0.00"));
+    }
+
+}
---
+++ Assets/Script/Other/View/TimeManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeManager {
+    
+    private bool m_isRun;
+    private float m_curTime;
+    private float m_triggerTime;
+    
+    public delegate void EventHandler();
+    public event EventHandler eventList;    //事件列表内的事件使用完一定要移除
+
+    public TimeManager(float second) {
+        m_curTime = 0.0f;
+        m_triggerTime = second;
+    }
+    
+    public void Start() {
+        m_isRun = true;
+    }
+    
+    public void Update(float deltaTime) {
+        if (m_isRun) {
+            m_curTime += deltaTime;
+            if (m_curTime > m_triggerTime) {
+                m_isRun = false;
+                m_curTime = 0.0f;
+                eventList();
+            }
+        }
+    }
+    
+    public void Stop() {
+        m_isRun = false;
+    }
+    
+    public void Continue() {
+        m_isRun = true;
+    }
+    
+    public void Restart() {
+        m_isRun = true;
+        m_curTime = 0.0f;
+    }
+    
+    public void ResetTriggerTime(float second) {
+        m_triggerTime = second;
+    }
+    
+}
